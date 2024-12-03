eee6025022e9f34c5dc1112ada566a2a5d09d93b
emmanueljustin <ejatienza01@gmail.com>
2024-12-03 13:30:38 +0800

    Added useLocalization custom hook

--- hooks/useLocalization.ts
+++ hooks/useLocalization.ts
@@ -1,5 +1,7 @@
+import { useEffect } from 'react';
 import '../i18n';
 import { useTranslation } from "react-i18next";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const useLocalization = () => {
   const { t, i18n } = useTranslation();
@@ -8,7 +10,20 @@ const useLocalization = () => {
     i18n.changeLanguage(lang);
   };
 
-  return { t, i18n, changeLanguage}
+  useEffect(() => {
+    const loadLanguage = async () => {
+      const storedLanguage = await AsyncStorage.getItem('lang');
+      if (storedLanguage) {
+        i18n.changeLanguage(storedLanguage);
+      }
+    };
+
+    loadLanguage();
+  }, [i18n]);
+
+  const currentLanguage = i18n.language as 'en' | 'ja';
+
+  return { t, i18n, changeLanguage, currentLanguage}
 }
 
 export default useLocalization
